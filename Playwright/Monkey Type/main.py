49a777999d3411a45485cbe5000d5fc223b3e250
Akashi <mohitsilwal16@gmail.com>
2025-12-28 18:36:16 +0530

    Monkey Type Automater Added, achieving 2.5K WPM MAX

---
+++ Playwright/Monkey Type/main.py
@@ -0,0 +1,48 @@
+import asyncio
+
+from os import system
+from playwright.async_api import Playwright, async_playwright
+
+system("cls")
+
+
+async def run(playwright: Playwright):
+    chromium = playwright.chromium
+    browser = await chromium.launch(headless=False)
+    page = await browser.new_page()
+
+    await page.goto("https://monkeytype.com", timeout=60 * 1000)
+
+    await page.locator("button.acceptAll").click()
+    await page.click("body")  # For Focus
+
+    await page.wait_for_selector("#words > div.word.active")
+
+    print("Started Typing ...")
+    while True:
+        try:
+            await page.wait_for_selector(".word:not(.typed)", timeout=2 * 1000) # 2 Sec
+        except:
+            print("Time Over ...")
+            break
+
+        words = await page.locator(".word:not(.typed)").all_inner_texts()
+        text = ""
+        for word in words:
+            text += word + " "
+            # text = f"{text} {word} " # Slow
+
+        # text = " ".join(words) + " " # Slow
+
+        await page.keyboard.type(text)
+
+    input("Press Enter to Exit ...")
+    await browser.close()
+
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+
+
+asyncio.run(main())
